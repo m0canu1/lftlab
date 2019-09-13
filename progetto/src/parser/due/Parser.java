740bf104bb61b0d9de658bd9ded9ec14c2ada147
m0canu1 <alexandrumocanu9@gmail.com>
2019-09-13 19:03:38 +0200

    aggiunto scheletro 3.2

--- progetto/src/parser/due/Parser.java
+++ progetto/src/parser/due/Parser.java
@@ -12,6 +12,7 @@ public class Parser {
     private Lexer lex;
     private BufferedReader pbr;
     private Token look;
+
     public Parser(Lexer l, BufferedReader br) {
         lex = l;
         pbr = br;
@@ -23,8 +24,8 @@ public class Parser {
      */
     void move() {
         look = lex.lexical_scan(pbr);
-        if(look != null) //evito di stampare "token = null"
-            System.out.println("token = " + look);
+        if (look != null) // evito di stampare "token = null"
+            System.err.println("token = " + look);
     }
 
     void error(String s) {
@@ -36,115 +37,115 @@ public class Parser {
      */
     void match(int t) {
         if (look.tag == t) {
-            if (look.tag != Tag.EOF) move();
-        } else error("syntax error");
+            if (look.tag != Tag.EOF)
+                move();
+        } else
+            error("syntax error");
     }
 
     /**
-     * Partenza.
-     * Gli unici caratteri accettati in partenza sono "(" o un Numero
+     * Partenza. Gli unici caratteri accettati in partenza sono "(" o un Numero S
+     * ::= E
      */
     public void start() throws NullPointerException {
-        if (look.tag == '(' || look.tag == Tag.NUM) 
+        if (look.tag == '(' || look.tag == Tag.NUM)
             expr();
         match(Tag.EOF);
     }
 
     /**
-     * (expr) ::= (term) (exprp)
+     * E ::= T E'
      */
     private void expr() {
+        if (look.tag == '(' || look.tag == Tag.NUM) {
             term();
             exprp();
+        } else
+            error("Syntax error");
     }
 
     /**
-     * (expr) ::= + (term) (exprp) 
-     * (expr)   | - (term) (exprp)
-     *         | ε
+     * E' ::= + T E' E' | - T E' | ε
      */
     private void exprp() {
         switch (look.tag) {
-            case '+':
-                match('+');
-                term();
-                exprp();
-                break;
-            case '-':
-                match('-');
-                term();
-                exprp();
-                break;
-            case ')':
-                break;
-            case Tag.EOF:
-                break;
-            default:
-                error("Syntax error");
-                break;
+        case '+':
+            match('+');
+            term();
+            exprp();
+            break;
+        case '-':
+            match('-');
+            term();
+            exprp();
+            break;
+        case ')':
+            break;
+        case Tag.EOF:
+            break;
+        default:
+            error("Syntax error");
+            break;
         }
     }
 
     /**
-     * (term) ::= (fact) (termp)
-     * può iniziare con ( o un numero
+     * T ::= F T' può iniziare con ( o un numero
      */
     private void term() {
+        if (look.tag == '(' || look.tag == Tag.NUM) {
             fact();
             termp();
+        } else
+            error("Syntax error");
     }
 
     /**
-     * () ::= * (fact)(termp)
-     *      | / (fact)(termp)
-     *      | ε
+     * T' ::= * F T' | / F T' | ε
      */
     private void termp() {
         switch (look.tag) {
-            case '*':
-                match('*');
-                fact();
-                termp();
-                break;
-            case '/':
-                match('/');
-                fact();
-                termp();
-                break;
-            case '+':
-                break;
-            case '-':
-                break;
-            case ')':
-                break;
-            case Tag.EOF:
-                break;
-            default:
-                error("Syntax error");
+        case '*':
+            match('*');
+            fact();
+            termp();
+            break;
+        case '/':
+            match('/');
+            fact();
+            termp();
+            break;
+        case '+':
+            break;
+        case '-':
+            break;
+        case ')':
+            break;
+        case Tag.EOF:
+            break;
+        default:
+            error("Syntax error");
         }
     }
 
     /**
-     * (fact) ::= ((expr)) | NUM
+     * F ::= (E) | NUM
      */
     private void fact() {
         switch (look.tag) {
-            case Tag.NUM:
-                match(Tag.NUM);
-                break;
-            case Tag.ID:
-                match(Tag.ID);
-                break;
-            default:
-                match('(');
-                expr();
-                if (look.tag == ')')
-                    match(')');
-                else
-                    error("Syntax error");
-                break;
+        case Tag.NUM: // ho letto un NUM
+            match(Tag.NUM);
+            break;
+        case '(': // ho letto una (
+            match(look.tag);
+            expr();
+            if (look.tag == ')') // dopo aver letto E devo trovare una ), altrimenti errore.
+                match(')');
+            else
+                error("Syntax error");
         }
     }
+
     public static void main(String[] args) {
         Lexer lex = new Lexer();
         String path = "progetto/parser.txt";
@@ -157,9 +158,8 @@ public class Parser {
         } catch (IOException e) {
             System.out.println("Errore del file. Nessun file con questo nome.");
             // e.printStackTrace();
-        }
-        catch (NullPointerException e) {
-            System.out.println("Letto simbolo non accettato.\nFine del programma.");            
+        } catch (NullPointerException e) {
+            System.out.println("Letto simbolo non accettato.\nFine del programma.");
             // e.printStackTrace();
         }
     }
