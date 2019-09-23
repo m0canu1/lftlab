98b33f1e1bdc1173b9276d5d0816dffba12bf4a4
m0canu1 <alexandrumocanu9@gmail.com>
2019-09-23 23:22:48 +0200

    aaa

--- lftlab/src/traduttore/test1.pas
+++ lftlab/src/traduttore/test1.pas
@@ -14,13 +14,3 @@ when (n>2) print(1)
 when (x>0) print(2)
 when (y>0) {print(y); print(3)}
 else print(5);
-
-read(x_1);
-read(_y2);
-
-while(x_1<>_y2){
-case
-when (x_1<_y2) {_y2:=_y2-x_1; print(_y2)}
-else {x_1:=x_1-_y2; print(x_1)}
-};
-print(x_1)
